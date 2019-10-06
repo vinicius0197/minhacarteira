70f449da95053e75fdb7e62d808085f7da753435
Vinicius Costa <viniciuscosta0197@gmail.com>
2019-10-05 21:39:48 -0300

    bugfix: fixing starter test

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,4 @@
 class Account < ApplicationRecord
-  has_many :transactions
+  has_many :transactions, dependent: :destroy
+  validates_presence_of :name, :balance
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
-  it { should have_many{:transactions}.dependent(:destroy) }
+  it { should have_many(:transactions).dependent(:destroy) }
 
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:balance) }
